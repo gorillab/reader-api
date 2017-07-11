aa13c6c751b56b3fdff3bd75a6b9134857139419
Khang Huynh <khanghuynh92@gmail.com>
2017-07-11 23:14:37 +0700

    feat(Close #41): Redirect url after login facebook successfully

--- controllers/AuthService.js
+++ controllers/AuthService.js
@@ -19,8 +19,7 @@ export const loginByFacebookCallback = (req, res, next) => {
       if (err2) {
         return new APIError('Authentication error', HttpStatus.UNAUTHORIZED, true);
       }
-
-      return res.json(req.user.securedInfo());
+      return res.redirect(`${process.env.REDIRECT_URL}?loggedInUser=${JSON.stringify(user.securedInfo())}`);
     });
   })(req, res, next);
 };
