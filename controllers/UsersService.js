b37b0cdd3fc729b9504978849ba5e9ae22b7dbdb
Khang Huynh <khanghuynh92@gmail.com>
2017-07-18 00:09:32 +0700

    feat(Close #46): implement get for-you posts

--- controllers/UsersService.js
+++ controllers/UsersService.js
@@ -94,3 +94,44 @@ export const getSubscriptions = async (req, res, next) => {
     next(err);
   }
 };
+
+
+export const forYou = async (req, res, next) => {
+  const args = req.swagger.params;
+
+  // get posts
+  const limit = args.limit.value || 25;
+  const page = args.page.value
+    ? (args.page.value > 0
+      ? args.page.value
+      : 1) - 1
+    : 0;
+  const sort = args.sort.value || 'title';
+  const query = {
+    isDeleted: false,
+    source: {
+      $in: req.user.sources,
+    },
+  };
+
+  if (args.query.value) {
+    query.$or = [
+      {
+        title: RegExp(args.query.value, 'i'),
+      },
+    ];
+  }
+
+  try {
+    const posts = await Post.list({
+      limit,
+      page,
+      sort,
+      query,
+    });
+
+    return res.json(posts);
+  } catch (err) {
+    return next(err);
+  }
+};
