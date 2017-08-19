1a86fffe84f9bb424cbe0d06a26a70e2526eb847
Khang Huynh <khanghuynh92@gmail.com>
2017-08-19 16:07:05 +0700

    refactor(Close #61): refactor newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -1,8 +1,9 @@
-// eslint-disable-next-line import/prefer-default-export
-export const config = {
+/* eslint-disable */
+Object.defineProperty(exports, '__esModule', {value: true}); // eslint-disable-next-line import/prefer-default-export
+var config = exports.config = {
   app_name: [process.env.APP_NAME],
   license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
-    level: 'info',
-  },
+    level: 'info'
+  }
 };
