cb8082eba08274fe04c027ec38fe1671d319bae3
Khang Huynh <khanghuynh92@gmail.com>
2017-08-19 18:12:43 +0700

    Update newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -1,9 +1,8 @@
 /* eslint-disable */
-Object.defineProperty(exports, '__esModule', {value: true}); // eslint-disable-next-line import/prefer-default-export
-var config = exports.config = {
+module.exports.config = {
   app_name: [process.env.APP_NAME],
   license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
-    level: 'info'
-  }
+    level: 'info',
+  },
 };
